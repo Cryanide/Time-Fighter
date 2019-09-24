e3efd66f3934e912a4193d46e6fbd338e560942a
Cryanide <addtext@hotmail.com>
2019-09-24 01:30:24 -0400

    inital commit
    just scripts

---
+++ Scripts/PowerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerControl : MonoBehaviour
+{
+    public GameObject SlowDownBubble;
+    Animator anim;
+    Vector3 pos;
+    Quaternion rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time slow bubble
+        pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        rotation = GameObject.FindGameObjectWithTag("Player").transform.rotation;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // SpawnBubble();
+            anim.SetTrigger("PowerBubble");
+        }
+
+        ////////////////////
+    }
+
+    public void SpawnBubble()
+    {
+        Instantiate(SlowDownBubble, pos, rotation);
+    }
+}
